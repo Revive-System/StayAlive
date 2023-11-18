83561161f0a9907d49c0fc7b8d51d7acd16cf8c2
Kex <kexanone@gmail.com>
2023-11-18 18:10:05 +0100

    Only add handling to players and make non-permanent protection an option

---
+++ scripts/Game/RSA/Components/Damage/SCR_CharacterDamageManagerComponent.c
@@ -0,0 +1,94 @@
+//#define RSA_STAY_ALIVE_FOREVER
+
+//------------------------------------------------------------------------------------------------
+modded class SCR_CharacterDamageManagerComponent : ScriptedDamageManagerComponent
+{
+	protected bool m_bRSA_Initialized = false;
+	protected HitZone m_pRSA_HealthHitZone;
+#ifndef RSA_STAY_ALIVE_FOREVER
+	protected const float RSA_UNCONSCIOUS_PROTECTION_TIMEOUT = 1000;
+#endif
+
+	//-----------------------------------------------------------------------------------------------------------
+	//! Add handlers for protecting players that fall unconscious
+	void RSA_Initialize(IEntity owner)
+	{
+		if (m_bRSA_Initialized)
+			return;
+		
+		SCR_CharacterControllerComponent charCtrl = SCR_CharacterControllerComponent.Cast(owner.FindComponent(SCR_CharacterControllerComponent));
+		if (!charCtrl)
+			return;
+		
+	#ifndef RSA_STAY_ALIVE_FOREVER
+		charCtrl.m_OnLifeStateChanged.Insert(RSA_OnLifeStateChanged);
+	#else
+		GetOnDamage().Insert(RSA_OnDamage_BloodHitZoneProtection);
+	#endif
+		GetOnDamage().Insert(RSA_OnDamage_HealthHitZoneProtection);
+		m_pRSA_HealthHitZone = GetHitZoneByName("Health");		
+		m_bRSA_Initialized = true;
+	}
+	
+#ifndef RSA_STAY_ALIVE_FOREVER
+	//-----------------------------------------------------------------------------------------------------------
+	//! Add/Remove protection handler when consciousness changes
+	protected void RSA_OnLifeStateChanged(ECharacterLifeState lifeState)
+	{
+		switch (lifeState)
+		{
+			// Add protection handler when revived
+			case ECharacterLifeState.ALIVE:
+			{
+				GetGame().GetCallqueue().Remove(RSA_EnableHealthHitZoneProtection);
+				RSA_EnableHealthHitZoneProtection(true);
+				break;
+			};
+			
+			// Schedule removal of protection handler when falling unconscious
+			case ECharacterLifeState.INCAPACITATED:
+			{
+				GetGame().GetCallqueue().CallLater(RSA_EnableHealthHitZoneProtection, RSA_UNCONSCIOUS_PROTECTION_TIMEOUT, false, false);
+				break;
+			};
+			
+			// Remove protection handler when dead
+			case ECharacterLifeState.DEAD:
+			{
+				GetGame().GetCallqueue().Remove(RSA_EnableHealthHitZoneProtection);
+				RSA_EnableHealthHitZoneProtection(false);
+				break;
+			};
+		};
+	}
+#else
+	//-----------------------------------------------------------------------------------------------------------
+	// Prevent the blood hit zone from getting destroyed
+	protected void RSA_OnDamage_BloodHitZoneProtection(EDamageType type, float damage, HitZone pHitZone, notnull Instigator instigator, inout vector hitTransform[3], float speed, int colliderID, int nodeID)
+	{
+		if (m_pBloodHitZone.GetHealth() < 1)
+			m_pBloodHitZone.SetHealth(1);
+	}
+#endif
+	
+	//-----------------------------------------------------------------------------------------------------------
+	//! Add/remove protection handler
+	void RSA_EnableHealthHitZoneProtection(bool enabled)
+	{
+		GetOnDamage().Remove(RSA_OnDamage_HealthHitZoneProtection);
+		
+		if (enabled)
+			GetOnDamage().Insert(RSA_OnDamage_HealthHitZoneProtection);
+	}
+	
+	//-----------------------------------------------------------------------------------------------------------
+	// Prevent the health hit zone from getting destroyed
+	protected void RSA_OnDamage_HealthHitZoneProtection(EDamageType type, float damage, HitZone pHitZone, notnull Instigator instigator, inout vector hitTransform[3], float speed, int colliderID, int nodeID)
+	{
+		if (m_pRSA_HealthHitZone.GetHealth() < 1)
+		{
+			m_pRSA_HealthHitZone.SetHealth(1);
+			m_pResilienceHitZone.SetHealth(1);
+		};
+	}
+}
